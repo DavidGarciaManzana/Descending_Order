fadc72b12ca747421a43a5d7b108a77ab4cd70ef
David Garcia <Davidgarciama@outlook.com>
2023-02-20 13:51:01 -0600

    Solution

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
-// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
-
-// Examples:
-// Input: 42145 Output: 54421
-
-// Input: 145263 Output: 654321
-
-// Input: 123456789 Output: 987654321
-
-export function descendingOrder(n: number): number {
-    // your code here
+let descendingOrder = (n: number): number => {
+    let stringNumber: string[] = n.toString().split('').sort((a: string, b: string) => b.localeCompare(a));
+    return parseInt(stringNumber.join(''))
 }
 
 //   Testing
